fb9b377aa44fdc40c0a74d28d10506a9f2dffe9f
sqoobe <sqoobe96@gmail.com>
2025-04-22 10:44:36 +0200

    10.45

--- test.js
+++ test.js
@@ -1,20 +1,53 @@
+const { dir } = require("console");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-fs.readFile(path.join(__dirname, "starter.txt"), "utf8", (err, data) => {
-  if (err) throw err;
-});
+// fs.readFile(path.join(__dirname, "starter.txt"), "utf8", (err, data) => {
+//   if (err) throw err;
+// });
 
-fs.writeFile(path.join(__dirname, "Greeting.txt"), "hi there, meow", (err) => {
-  if (err) throw err;
-  console.log("Done writing!");
-});
+// fs.writeFile(path.join(__dirname, "Greeting.txt"), "hi there, meow", (err) => {
+//   if (err) throw err;
+//   console.log("Done writing!");
+// });
 
-fs.writeFile(path.join(__dirname, "Appended.txt"), "mjau mjau, meow", (err) => {
-  if (err) throw err;
-  console.log("Appended");
-});
+// fs.writeFile(path.join(__dirname, "Appended.txt"), "mjau mjau, meow", (err) => {
+//   if (err) throw err;
+//   console.log("Appended");
+// });
+
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "starter.txt"),
+      "utf8"
+    );
+    console.log(data);
+
+    await fsPromises.unlink(path.join(__dirname, "starter.txt"));
+
+    await fsPromises.writeFile(path.join(__dirname, "newFile.txt"), data);
+
+    await fsPromises.appendFile(
+      path.join(__dirname, "newFile.txt"),
+      "this is some new data"
+    );
+
+    await fsPromises.rename(
+      path.join(__dirname, "newFile.txt"),
+      path.join(__dirname, "secondName.txt")
+    );
+
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "secondName.txt"),
+      "utf8"
+    );
+    console.log(newData);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 process.on("uncaughtException", (err, origin) => {
   fs.writeSync(
