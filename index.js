64db7f10f3763cc0fe2ee1c3d4ab73b5a2fa2cb8
sqoobe <sqoobe96@gmail.com>
2025-04-28 09:46:14 +0200

    09.45 - 04/28/25

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "view", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page.html");
+});
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
