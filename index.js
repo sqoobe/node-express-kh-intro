b4e0a39436448fd417268880ce053c311306f2de
sqoobe <sqoobe96@gmail.com>
2025-04-28 11:07:58 +0200

    11.05

--- index.js
+++ index.js
@@ -1,24 +1,59 @@
+//imports
 const path = require("path");
 const express = require("express");
+const { logger } = require("./middleware/logEvents");
+const cors = require("cors");
 
 const app = express();
 
-app.get("^/$|/index(.html)?", (req, res) => {
+const PORT = process.env.PORT || 3500;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.use(logger);
+app.use(cors());
+
+const whitelist = ["http://127.0.0.1:5500", "http://localhost:3500"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Blocked by CORS!"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.get(/^\/$|\/index(\.html)?$/, (req, res) => {
   res.sendFile(path.join(__dirname, "view", "index.html"));
 });
 
-app.get("/new-page(.html)?", (req, res) => {
+app.get(["/new-page", "/new-page.html"], (req, res) => {
   res.sendFile(path.join(__dirname, "view", "new-page.html"));
 });
 
-app.get("/old-page(.html)?", (req, res) => {
+app.get(["/old-page", "/old-page.html"], (req, res) => {
   res.redirect(301, "/new-page.html");
 });
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "view", "404.html"));
+app.get("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "view", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 JSON not found" });
+  } else req.accepts("txt");
+  {
+    res.type({ error: "404 text not found" });
+  }
 });
 
-const PORT = process.env.PORT || 3500;
+app.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send(err.message);
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ middleware/logEvents.js
@@ -0,0 +1,7 @@
+const logger = (req, res, next) => {
+  //   LogEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logger };
