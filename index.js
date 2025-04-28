c00ddc0307d569a8510a2b50b0476d784edb6d4a
sqoobe <sqoobe96@gmail.com>
2025-04-28 09:07:58 +0200

    09.05 - 04/28/25

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
-
-console.log(uuid());
-console.log(format(new Date(), "ddMMyyyy\tHH:mm:ss"));
-
-console.log("Server restarted!");
+const path = require("path");
